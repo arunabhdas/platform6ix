f369550f1340ca2926df59d58de2f1842b0ae859
Arunabh Das <arunabhdas@gmail.com>
2021-08-26 14:54:21 -0400

    Improvements to DashboardActivity

--- platform6ix-android/app/src/main/java/com/platform6ix/platform6ix/activities/DashboardActivity.kt
+++ platform6ix-android/app/src/main/java/com/platform6ix/platform6ix/activities/DashboardActivity.kt
@@ -2,21 +2,24 @@ package com.platform6ix.platform6ix.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
+import android.os.strictmode.NonSdkApiUsedViolation
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.platform6ix.platform6ix.R
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
 
         // Pass each menuId as a set of Ids because each menu
         // should be considered as a top level destination
@@ -28,7 +31,7 @@ class DashboardActivity : AppCompatActivity() {
                 R.id.navigation_notifications
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
 
         navView.setupWithNavController(navController)
     }
