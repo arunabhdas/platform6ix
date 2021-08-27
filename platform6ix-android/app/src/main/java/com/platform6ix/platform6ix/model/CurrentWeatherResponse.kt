f913dc4447d693746e028094efcbec2da8e71088
Arunabh Das <arunabhdas@gmail.com>
2021-08-27 00:25:55 -0400

    Centralizing models in a single file

--- platform6ix-android/app/src/main/java/com/platform6ix/platform6ix/model/CurrentWeatherResponse.kt
+++ platform6ix-android/app/src/main/java/com/platform6ix/platform6ix/model/CurrentWeatherResponse.kt
@@ -1,5 +1,7 @@
 package com.platform6ix.platform6ix.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 
@@ -8,4 +10,59 @@ data class CurrentWeatherResponse(
     val currentWeatherEntry: CurrentWeatherEntry,
     val location: Location,
     val request: Request
+)
+
+const val CURRENT_WEATHER_ID = 0
+@Entity(tableName = "current_weather")
+data class CurrentWeatherEntry(
+    val cloudcover: Double,
+    val feelslike: Double,
+    val humidity: Double,
+    @SerializedName("is_day")
+    val isDay: String,
+    @SerializedName("observation_time")
+    val observationTime: String,
+    val precip: Double,
+    val pressure: Double,
+    val temperature: Double,
+    @SerializedName("uv_index")
+    val uvIndex: Double,
+    val visibility: Double,
+    @SerializedName("weather_code")
+    val weatherCode: Double,
+    @SerializedName("weather_descriptions")
+    val weatherDescriptions: List<String>,
+    @SerializedName("weather_icons")
+    val weatherIcons: List<String>,
+    @SerializedName("wind_degree")
+    val windDegree: Double,
+    @SerializedName("wind_dir")
+    val windDir: String,
+    @SerializedName("wind_speed")
+    val windSpeed: Double
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = CURRENT_WEATHER_ID
+}
+
+data class Location(
+    val country: String,
+    val lat: String,
+    val localtime: String,
+    @SerializedName("localtime_epoch")
+    val localtimeEpoch: Int,
+    val lon: String,
+    val name: String,
+    val region: String,
+    @SerializedName("timezone_id")
+    val timezoneId: String,
+    @SerializedName("utc_offset")
+    val utcOffset: String
+)
+
+data class Request(
+    val language: String,
+    val query: String,
+    val type: String,
+    val unit: String
 )
