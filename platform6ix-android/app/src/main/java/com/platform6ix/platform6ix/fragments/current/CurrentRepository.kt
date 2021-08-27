b38c0f52aa11090370531150c1d424704b250cfa
Arunabh Das <arunabhdas@gmail.com>
2021-08-26 22:25:31 -0400

    Improvements to CurrentWeatherEntry class

--- platform6ix-android/app/src/main/java/com/platform6ix/platform6ix/fragments/current/CurrentRepository.kt
+++ platform6ix-android/app/src/main/java/com/platform6ix/platform6ix/fragments/current/CurrentRepository.kt
@@ -1,10 +1,10 @@
-package com.platform6ix.platform6ix.repository
+package com.platform6ix.platform6ix.fragments.current
 
 import com.platform6ix.platform6ix.services.RetrofitService
 
 /**
  * Created by Das on 2021-08-26.
  */
-class MainRepository constructor(private val retrofitService: RetrofitService){
+class CurrentRepository constructor(private val retrofitService: RetrofitService){
     suspend fun getCurrentWeather() = retrofitService.getCurrentWeather("Miami", "en")
 }
--- platform6ix-android/app/src/main/java/com/platform6ix/platform6ix/fragments/current/CurrentViewModel.kt
+++ platform6ix-android/app/src/main/java/com/platform6ix/platform6ix/fragments/current/CurrentViewModel.kt
@@ -1,7 +1,13 @@
 package com.platform6ix.platform6ix.fragments.current
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CurrentViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
 }
